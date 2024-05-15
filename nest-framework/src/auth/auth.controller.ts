f4586faecefbbe104259206f2516883132ccefa2
xlgelingen <xuliucg@gmail.com>
2024-05-15 18:45:46 +0800

    feat: modify nest

--- nest-framework/src/auth/auth.controller.ts
+++ nest-framework/src/auth/auth.controller.ts
@@ -26,9 +26,8 @@ export class AuthController {
   }
 
   @Public()
-  @HttpCode(HttpStatus.OK) //设置了 HTTP 响应状态码为 200，没这个的话，会是201
+  @HttpCode(HttpStatus.OK)
   @Post('login')
-  //Record<string, any> 是 TypeScript 中的一种类型声明，用于表示一个键值对的对象，其中键的类型是 string，值的类型是 any。
   signInByWX(@Body() signInDto: Record<string, any>) {
     return this.authService.signInByWX(signInDto);
   }
